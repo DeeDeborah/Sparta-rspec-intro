e53e3c8a19e112d32dd471e5bfb216af1691f940
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2018-04-09 15:56:37 +0100

    first commit

---
+++ rspec_intro.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+
+describe 'this is the description of the product you are going to test' do
+
+    context 'context is merely another way to break out your code and description' do
+
+        it 'the it block is where the tests begin and gets run' do
+          # it 'should return true' do
+
+    end
+  end
+
+end
+
+describe 'a simple test' do
+
+  it 'should be able to check values' do
+    a = 1
+    b = 2
+    c = true
+  end
+
+end
