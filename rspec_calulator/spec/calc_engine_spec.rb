8cd8e1640c74fc4f2ca85bcef7648607e6433223
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2018-04-10 10:06:05 +0100

    added fizzBuzz folders and file

--- rspec_calulator/spec/calc_engine_spec.rb
+++ rspec_calulator/spec/calc_engine_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 
 
 describe Calcengine do
-     before(:each) do
-        @calc = Calcengine.new
-      end
+  before(:each) do
+    @calc = Calcengine.new
+  end
 
 
   describe 'add' do
     it 'should correctly add two numbers' do
-    expect(@calc.add(1,1)).to eql(2)
-  end
+      expect(@calc.add(1,1)).to eql(2)
+    end
 
-  describe 'subtract' do
+    describe 'subtract' do
       it 'should correctly subtract two numbers' do
-      expect(@calc.subtract(6,3)).to eql(3)
+        expect(@calc.subtract(6,3)).to eql(3)
       end
     end
 
-      describe 'divide' do
-          it 'should correctly divide two numbers' do
-          expect(@calc.divide(12,2)).to eql(6)
-          end
-        end
+    describe 'divide' do
+      it 'should correctly divide two numbers' do
+        expect(@calc.divide(12,2)).to eql(6)
+      end
+    end
 
-      describe 'mutlipy' do
-          it 'should correctly multiply two numbers' do
-          expect(@calc.multiply(12,2)).to eql(24)
-          end
-        end
+    describe 'multiply' do
+      it 'should correctly multiply two numbers' do
+        expect(@calc.multiply(12,2)).to eql(24)
+      end
+    end
 
   end
 end
